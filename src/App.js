f22eb1eff5f2e0a91e65f1673aebd2cb181ff8a4
jxhnkndl <jkroyston@gmail.com>
2022-08-11 19:29:14 -0400

    create containers and establish basic page structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(15, 15, 15, 1);
+  color: #fff;
+`;
+
+const InnerContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2em;
+`;
+
+const Content = styled.h2`
+  color: #fff;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Navbar />
-      <h1>HELLO</h1>
-    </div>
+      <InnerContainer>
+        <Content>ANIMTED NAVBAR!</Content>
+      </InnerContainer>
+    </AppContainer>
   );
 }
 
