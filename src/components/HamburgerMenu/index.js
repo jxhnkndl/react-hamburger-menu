6fa6fa1b8c1273636aa58f168010c617de9073f5
jxhnkndl <jkroyston@gmail.com>
2022-08-11 20:46:13 -0400

    design animation for sliding menu container open and closed

--- src/components/HamburgerMenu/index.js
+++ src/components/HamburgerMenu/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuToggle from '../MenuToggle/MenuToggle';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 // icon container
 const HamburgerMenuContainer = styled.div`
   display: flex;
 `;
 
-// menu container (the div that slides out from the right when menu is engaged)
-const MenuContainer = styled.div`
+// menu container (the div that slides out from the right when menu is engaged)'
+// styling the motion.div makes the menu capable of being animated
+const MenuContainer = styled(motion.div)`
   width: 100%;
   min-width: 300px;
   max-width: 44%;
@@ -19,14 +21,50 @@ const MenuContainer = styled.div`
   position: fixed;
   top: 0;
   right: 0;
+  transform: translateX(4em)
   user-select: none;
   padding: 1em 2.5em;
 `;
 
+const menuVariants = {
+  open: {
+    transform: 'translateX(3%)',
+  },
+  closed: {
+    transform: 'translateX(103%)',
+  },
+};
+
+// animation properties for animating the menu container
+const menuTransition = {
+  type: 'spring',
+  duration: 1,
+  stiffness: 33,
+  // the delay accounts for the hamburger icon animation
+  delay: 0.1,
+};
+
 const HamburgerMenu = (props) => {
+  // set menu's open/closed state
+  const [isOpen, setOpen] = useState(false);
+
+  // toggle menu state between open and closed
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <HamburgerMenuContainer>
-      <MenuToggle toggle={() => {}} isOpen={false} />
+      {/* pass toggling function and open state to the icon */}
+      <MenuToggle toggle={toggleMenu} isOpen={isOpen} />
+      <MenuContainer
+        initial="false"
+        animate={isOpen ? 'open' : 'closed'}
+        variants={menuVariants}
+        transition={menuTransition}
+      >
+        MENU
+      </MenuContainer>
     </HamburgerMenuContainer>
   );
 };
